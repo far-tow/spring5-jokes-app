5f029786b144081d232f09f9265a19949215c7af
Farhad Towfighian <fartow@gmail.com>
2023-01-02 19:41:16 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,14 @@
+package se.lexicon.spring5jokesapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/se/lexicon/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package se.lexicon.spring5jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
